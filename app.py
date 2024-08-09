486d229935a8a885b0b84d08ecc0759c9f834eba
anweshchandekar <149099211+anweshchandekar@users.noreply.github.com>
2024-08-10 02:09:46 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,15 +1,9 @@
+import os
 import tensorflow as tf
 import gradio as gr
 
-# Load the model using a relative path
+# Adjust the path to where the model file is located in the Hugging Face environment
 model_path = r'C:\Users\anwes\OneDrive\Desktop\AI_Music_Generator\scripts\AI_Music_Generator.h5'
+
+# Load the model
 model = tf.keras.models.load_model(model_path)
-
-def generate_music(seed_sequence):
-    # Placeholder for music generation logic
-    # Convert seed_sequence to the required format
-    generated_sequence = model.predict(seed_sequence)
-    return generated_sequence
-
-iface = gr.Interface(fn=generate_music, inputs="text", outputs="text")
-iface.launch()
